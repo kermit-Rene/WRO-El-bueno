9af1a426af6635b0c990c85e7cea59c4619b088a
kermit-Rene <R9E5NE@GMAIL.COM>
2026-04-01 12:31:06 -0700

    Incorporacion de arduino

---
+++ ejmploblink1/ejmploblink1.ino
@@ -0,0 +1,13 @@
+int tiempo=500;
+void setup() {
+  // put your setup code here, to run once:
+pinMode(12,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+digitalWrite(13,HIGH);
+delay(x);
+digitalWrite(13,LOW);
+delay(x);
+}
